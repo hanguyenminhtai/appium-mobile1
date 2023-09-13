01cb89c6fa5b8c82baff0dd5158e14419520cc02
ha tai <hanguyenminhtai@gmail.com>
2023-09-13 14:25:25 +0700

    lesson-1

---
+++ src/test/java/api_learning/lesson_1.java
@@ -0,0 +1,35 @@
+package api_learning;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class lesson_1 {
+
+    public static void main(String[] args) {
+
+        AppiumDriver<MobileElement> appiumDriver = null;
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability("udid", "emulator-5554");
+        desiredCapabilities.setCapability("appPackage", "com.wdiodemoapp");
+        desiredCapabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+
+        try {
+            // Init appium session
+            URL appiumServer = new URL("http://localhost:4723/wd/hub");
+            appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCapabilities);
+
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (appiumDriver != null)
+            appiumDriver.quit();
+    }
+}
