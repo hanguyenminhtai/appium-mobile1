2e8062305188a7e75b52788e388bdc52744ceb06
ha tai <hanguyenminhtai@gmail.com>
2023-09-13 16:18:34 +0700

    lesson-2

--- src/test/java/api_learning/LaunchAppTest.java
+++ src/test/java/api_learning/LaunchAppTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
 
-public class lesson_1 {
+public class LaunchAppTest {
 
     public static void main(String[] args) {
 
