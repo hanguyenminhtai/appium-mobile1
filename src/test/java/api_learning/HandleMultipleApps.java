ed45b150d4facf6bbebde097c2f61ff1c46c235e
ha tai <hanguyenminhtai@gmail.com>
2023-09-14 22:11:35 +0700

    repair

--- src/test/java/api_learning/HandleMultipleApps.java
+++ src/test/java/api_learning/HandleMultipleApps.java
@@ -5,7 +5,11 @@ import driver.DriverFactory;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import platform.Platform;
 
 import java.time.Duration;
@@ -22,6 +26,29 @@ public class HandleMultipleApps {
 
             MobileElement emailInputElem = appiumDriver.findElement(MobileBy.AccessibilityId("input-email"));
             MobileElement passwordInputElem = appiumDriver.findElement(MobileBy.AccessibilityId("input-password"));
+            Dimension screenDimension = appiumDriver.manage().window().getSize();
+            int screenHeight = screenDimension.getHeight();
+            int screenWidth = screenDimension.getWidth();
+
+            //cal the start/end point
+            int xStartPoint = 50 * screenWidth / 100;
+            int xEndPoint = 50 * screenWidth / 100;
+
+            int yStartPoint = 50 * screenHeight / 100;
+            int yEndPoint = 10 * screenHeight / 100;
+
+            PointOption startPoint = new PointOption().withCoordinates(xStartPoint, yStartPoint);
+            PointOption endPoint = new PointOption().withCoordinates(xEndPoint, yEndPoint);
+
+            //using touch action to swipe
+            TouchAction touchAction = new TouchAction(appiumDriver);
+
+            touchAction
+                    .press(startPoint)
+                    .waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+                    .moveTo(endPoint)
+                    .release()
+                    .perform();
             MobileElement loginBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("button-LOGIN"));
 
             emailInputElem.sendKeys("robert@gmail.com");
@@ -40,14 +67,14 @@ public class HandleMultipleApps {
             By netLabelSel = MobileBy.xpath("//*[@text='Network & internet']");
             appiumDriver.findElement(netLabelSel).click();
 
-            By wifiLabelSel = MobileBy.xpath("//*[@text='Wi-fi']");
+            By wifiLabelSel = MobileBy.xpath("//*[@text='Wi‑Fi']");
             appiumDriver.findElement(wifiLabelSel).click();
 
             //Toggle on/off
-            By wifiStatusSel = MobileBy.id("com.android.settings:id/switchWidget");
+            By wifiStatusSel = MobileBy.id("com.android.settings:id/switch_text");
             MobileElement wifiStatusElem = appiumDriver.findElement(wifiStatusSel);
             String wifiStatus = wifiStatusElem.getText().trim();
-            if (wifiStatus.equalsIgnoreCase("on"))
+            if (wifiStatus.equalsIgnoreCase("Use Wi‑Fi"))
                 wifiStatusElem.click();
 
             //Come back to the app > interact with other elems
